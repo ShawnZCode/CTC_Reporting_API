0627fe58507cbea0edb4c9aee0265fc0db54d4bb
Shawn Zirbes <shawn.zirbes@gmail.com>
2024-02-13 15:53:02 -0600

    Created DB Connection and first table
    Updated DB Connection to leverage SQLAlchemy Module

--- SQL_Connection/db_connection.py
+++ SQL_Connection/db_connection.py
@@ -1,9 +1,13 @@
 """Class used to create a database and return a database connection"""
 import json
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy.ext.declarative import declarative_base
 
-from JSON.Functions.read_file import read_file
+## Establish the async connection to the database for both postgres and mssql
 
-Base = declarative_base()
+class NotFoundError(Exception):
+    pass
+
+class Base(DeclarativeBase):
+    pass
