cf487728bee26a59256c57a4cd14ffe317a13fd8
ShawnZCode <shawn.zirbes@gmail.com>
2024-04-24 17:26:34 -0500

    SQL_Connection V1.1.1
    Continued development on table creation and data population

--- SQL_Connection/Tables/tbl_cms_contentAttachments.py
+++ SQL_Connection/Tables/tbl_cms_contentAttachments.py
@@ -6,15 +6,13 @@ from pydantic import BaseModel
 from sqlalchemy import Boolean, DateTime, Float, ForeignKey, Integer, String, Uuid
 from sqlalchemy.orm import Mapped, Session, mapped_column
 
-from APICore.result_models.cms.content_libraries import CMSContentLibrary
-from APICore.result_models.cms.libraries import CMSLibrary, CMSLibraryBase
+from APICore.result_models.cms.content_attachments import CMSContentAttachment
 from SQL_Connection.db_connection import Base, NotFoundError, SessionLocal
-from SQL_Connection.tables.tbl_cms_contentLibraries import create_new_content_library
 
 
 ## Using SQLAlchemy2.0 generate Table with association to the correct schema
-class TblCMSLibraries(Base):
-    __tablename__ = "libraries"
+class TblCMSContentAttachments(Base):
+    __tablename__ = "contentAttachments"
     __table_args__ = {"schema": "cms"}
 
     id: Mapped[uuid4] = mapped_column(
@@ -28,55 +26,61 @@ class TblCMSLibraries(Base):
     updatedById: Mapped[uuid4] = mapped_column(
         ForeignKey("accounts.users.id"), nullable=False
     )
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    type: Mapped[str] = mapped_column(String(15), nullable=False)
+    fileExtension: Mapped[str] = mapped_column(String(10), nullable=True)
+    fileSizeinBytes: Mapped[int] = mapped_column(Integer(), nullable=True)
+    contentId: Mapped[uuid4] = mapped_column(
+        ForeignKey("cms.contents.id"), nullable=False
+    )
     description: Mapped[str] = mapped_column(String(2048), nullable=True)
-    uploadContent: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    defaultRole: Mapped[str] = mapped_column(String(15), nullable=False)
-    imageUri: Mapped[str] = mapped_column(String(2048), nullable=True)
+    isLink: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(2048), nullable=False)
+    path: Mapped[str] = mapped_column(String(2048), nullable=False)
+    type: Mapped[int] = mapped_column(String(100), nullable=False)
     refreshedId: Mapped[uuid4] = mapped_column(
         ForeignKey("core.refreshed.id"), nullable=False
     )
 
 
 ## function to write to create a new entry item in the table
-def create_new_library(
-    item: CMSLibrary, refreshed, session: Session = None
-) -> CMSLibrary:
-    base_item = CMSLibraryBase(**item.model_dump(exclude_defaults=True))
-    new_entry = TblCMSLibraries(
-        **base_item.model_dump(exclude_defaults=True), refreshedId=refreshed.id
+def create_new_attachment(
+    item: CMSContentAttachment, refreshed
+) -> CMSContentAttachment:
+    base_attachment = CMSContentAttachment(**item.model_dump())
+    base_attachment.refreshedId = refreshed.id
+    new_entry = TblCMSContentAttachments(
+        **base_attachment.model_dump(exclude_none=True)
     )
-    if session is None:
-        db = SessionLocal()
+    db = SessionLocal()
     try:
-        new_entry = read_db_library(item, db)
+        new_entry = read_db_attachment(item, db)
     except NotFoundError:
         db.add(new_entry)
         db.commit()
         db.refresh(new_entry)
-        if item.contentLibraries != []:
-            [create_new_content_library(i, refreshed) for i in item.contentLibraries]
     finally:
         db.close()
     return new_entry
 
 
 ## function to read from the table
-def read_db_library(item: CMSLibrary, session: Session) -> CMSLibrary:
-    db_item = (
-        session.query(TblCMSLibraries).filter(TblCMSLibraries.id == item.id).first()
+def read_db_attachment(
+    item: CMSContentAttachment, session: Session
+) -> CMSContentAttachment:
+    db_attachment = (
+        session.query(TblCMSContentAttachments)
+        .filter(TblCMSContentAttachments.id == item.id)
+        .first()
     )
-    if db_item is None:
-        raise NotFoundError
-    return db_item
+    if db_attachment is None:
+        raise NotFoundError(f"AttachmentId: {item.id} not found")
+    return db_attachment
 
 
 ## function to update the table
-def update_library():
+def update_entry():
     pass
 
 
 ## function to delete from the table
-def delete_library():
+def delete_entry():
     pass
---
+++ SQL_Connection/Tables/tbl_cms_contentFileComponentProperties.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel
+from sqlalchemy import Boolean, DateTime, Float, ForeignKey, Integer, String, Uuid
+from sqlalchemy.orm import Mapped, Session, mapped_column
+
+from APICore.result_models.cms.content_file_component_properties import (
+    CMSContentFileComponentProperty,
+)
+from SQL_Connection.db_connection import Base, NotFoundError, SessionLocal
+
+
+## Using SQLAlchemy2.0 generate Table with association to the correct schema
+class TblCMSContentFileComponentProperties(Base):
+    __tablename__ = "contentFileComponentProperties"
+    __table_args__ = {"schema": "cms"}
+
+    id: Mapped[uuid4] = mapped_column(
+        Uuid(), primary_key=True, index=True, nullable=False
+    )
+    contentFileComponentId: Mapped[uuid4] = mapped_column(
+        ForeignKey("cms.contentFileComponents.id"), nullable=False
+    )
+    isInstance: Mapped[bool] = mapped_column(Boolean(), nullable=True)
+    isReadOnly: Mapped[bool] = mapped_column(Boolean(), nullable=True)
+    name: Mapped[str] = mapped_column(String(2048), nullable=True)
+    revitParameterGroupId: Mapped[int] = mapped_column(Integer(), nullable=True)
+    revitSharedParameterGuid: Mapped[uuid4] = mapped_column(Uuid(), nullable=True)
+    revitStorageTypeId: Mapped[int] = mapped_column(Integer(), nullable=True)
+    revitDisplayUnitTypeId: Mapped[int] = mapped_column(Integer(), nullable=True)
+    doubleValue: Mapped[float] = mapped_column(Float(), nullable=True)
+    type: Mapped[str] = mapped_column(String(100), nullable=True)
+    value: Mapped[str] = mapped_column(String(2048), nullable=True)
+    unitTypeIdVersionless: Mapped[str] = mapped_column(String(2048), nullable=True)
+    refreshedId: Mapped[uuid4] = mapped_column(
+        ForeignKey("core.refreshed.id"), nullable=False
+    )
+
+
+## function to write to create a new entry item in the table
+def create_new_property(
+    item: CMSContentFileComponentProperty,
+    refreshed,
+    session: Session = None,
+) -> CMSContentFileComponentProperty:
+    base_property = CMSContentFileComponentProperty(**item.model_dump())
+    base_property.refreshedId = refreshed.id
+    new_entry = TblCMSContentFileComponentProperties(
+        **base_property.model_dump(),
+    )
+    db = SessionLocal()
+    try:
+        new_entry = read_db_property(item, db)
+    except NotFoundError:
+        db.add(new_entry)
+        db.commit()
+        db.refresh(new_entry)
+    finally:
+        db.close()
+    return new_entry
+
+
+## function to read from the table
+def read_db_property(
+    item: CMSContentFileComponentProperty, session: Session
+) -> CMSContentFileComponentProperty:
+    db_content = (
+        session.query(TblCMSContentFileComponentProperties)
+        .filter(TblCMSContentFileComponentProperties.id == item.id)
+        .first()
+    )
+    if db_content is None:
+        raise NotFoundError(f"PropertyId: {item.id} not found")
+    return db_content
+
+
+## function to update the table
+def update_entry():
+    pass
+
+
+## function to delete from the table
+def delete_entry():
+    pass
